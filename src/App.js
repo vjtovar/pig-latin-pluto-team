b9810174bf0140f0d9729324a284d306c4bbaeca
lancewrites <lancewrites77@gmail.com>
2022-05-25 16:46:28 -0700

    added more stuff

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component{
     this.state = {
       // "phrase" is the text entered by the user - right now there are test words hard coded to make the process of testing your code faster and easier
       // ACTION ITEM: when you are ready for your full user experience, delete the test words so phrase is assigned an empty string
-      phrase: "alpha through yummy squeal queen fry",
+      phrase: "lowercase please, 'cause we ain't that good at coding yet",
       // "phraseTranslated" is what the user will see appear on the page as Pig Latin, it starts as the preset message and updates when your user clicks the "submit" button
       phraseTranslated: "This is where your translated sentence will appear."
     }
@@ -34,35 +34,45 @@ class App extends Component{
       console.log("vowelsArray:", vowelsArray)
 
       // your code here!
+      //variable called vowelInd that is assigned the value of the first vowel
       let vowelInd = currentWord.indexOf(vowelsArray[0])
       console.log("vowelInd", vowelInd)
+      //variable called quInd that is assigned the value of the first instance of a q
       let quInd = currentWord.indexOf("qu")
+      //variable called uInd that shows the index of the "u" after a "q"
       let uInd = quInd + 1
+      //variable called yInd that takes note of where the "y" is 
       let yInd = currentWord.indexOf("y")
       console.log("O" == "o")
       console.log("quInd", quInd)
       console.log("uInd", uInd)
       console.log("yInd", yInd)
+      //if the first letter of the word is alwo the word's first vowel, then add "way" to the end of it.
       if (currentWord[0] === vowelsArray[0]) {
         currentWord = currentWord + "way";
         
       } 
+      //if the first vowel is at the end of a two-letter word, then put the first letter after the vowel and add "ay" to that
       else if(vowelInd === 1 && (currentWord.length === 2)){
         currentWord = currentWord.substring(vowelInd) + currentWord.charAt(0) +"ay"
         }
+       //if the first letter of the word is "q" and a "u" is the second letter, then put the "qu" at the end of the word and add "ay" to the "qu" 
       else if(currentWord[0] === (currentWord[quInd])){
         currentWord = currentWord.substring(uInd + 1) + currentWord.substring(0, 2)+ "ay"
       }
+      //if "qu" is the second letter (usaully after a consonant) then start the new word with the letter after the "u" and add the consonant + "quay" to the end
       else if(quInd === 1){
         currentWord = currentWord.substring(uInd +1) + currentWord.substring(0, uInd +1) +"ay"
       }
+      //if there is no normal vowel, and there is a "y" at the end of the word, put the letters that were originally before the "y" after it and add "ay" to that
       else if(vowelInd === -1 && currentWord.indexOf("y") === (currentWord.length -1)){
         currentWord = currentWord.substring(yInd) + currentWord.substring(0 , yInd) +"ay"
       }
+      //if the typical vowel is at the end of the word and there is a "y" that is two letters before that typical vowel, put the letters that were before that "y" at the end of the word and add "ay"
       else if((vowelInd === currentWord.length -1) && (yInd === vowelInd -2)){
       currentWord = currentWord.substring(yInd) + currentWord.substring(0, yInd) +"ay"
       }
-
+      //if the word doesn't meet the above cases then it most likely is a simple word, so if the first letter is not a vowel, then put the letters before the first vowel at the end and add "ay"
       else if(currentWord[0] !== vowelsArray[0]){
         
         currentWord = currentWord.substring(vowelInd) + currentWord.substring(0, vowelInd) + "ay"
@@ -98,7 +108,7 @@ class App extends Component{
     // this method restarts the game by setting the original state
     // ACTION ITEM: when you are ready for your full user experience, delete the test words in phrase so that is assigned an empty string
     this.setState({
-      phrase: "alpha through yummy squeal queen fry",
+      phrase: "lowercase please, 'cause we ain't that good at coding yet",
       phraseTranslated: "This is where your translated sentence will appear."
     })
   }
@@ -136,11 +146,11 @@ class App extends Component{
           />
           <br />
           {/* button that called the setUpPreventDefault method which calls the myPigLatinCodeHere method */}
-          <button onClick={this.setUpPreventDefault}>Submit</button>
-          <button onClick={this.restartGame}>Clear</button>
+          <button onClick={this.setUpPreventDefault}>Tell Me</button>
+          <button onClick={this.restartGame}>Un-Tell Me</button>
         </div>
         <p className="outputArea" >{this.state.phraseTranslated}</p>
-        <footer className="footerText">Coded by ~Valerie....Lance~</footer>
+        <footer className="footerText">Coded by ~Valerie....Lance~, y'all <br></br> **Don helped a lot. Thanks be to the Don**</footer>
       </>
     )
   }
